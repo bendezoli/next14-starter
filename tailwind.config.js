4e9df986074db3c8d24ad8686869c02f59cfc4d2
bendezoli <zoltan@octopus-digital.pro>
2024-04-22 14:43:01 +0300

    some basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem", // 40px
+        sm: "1rem", // 16px
+        md: "1.5rem", // 24px
+        xl: "2rem", // 32px
+        xxl: "2.5rem", // 40px
+      },
+    },
+
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1440px",
+      xxxl: "1920px",
+      ultraWide: "2500px",
+      short: { raw: "(max-height: 800px)" },
+    },
+
     extend: {},
   },
   plugins: [],
