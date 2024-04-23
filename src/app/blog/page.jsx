64cfb6081ef8904724878cfb4298a18ce5034ac7
bendezoli <zoltan@octopus-digital.pro>
2024-04-23 14:46:56 +0300

    blog added

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
 
-const BlogPage = () => {
-  return <div>Blog page</div>;
+const BlogPage = async () => {
+  return (
+    <div className={styles.container}>
+      <div className={"w-full flex flex-wrap gap-4"}>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = ({ post }) => {
+  return (
+    <div className={(styles.container, `w-full md:w-[48%] lg:w-[32%]`)}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image src="/png/noavatar.png" alt="" fill className={styles.img} />
+        </div>
+
+        {/* <span className={styles.date}>
+          {post.createdAt?.toString().slice(4, 16)}
+        </span> */}
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>This is a title</h1>
+        <p className={styles.desc}>lorem ipsum dolor sitamet</p>
+        <Link className={styles.link} href={"/"}>
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
