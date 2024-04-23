e386909daeb11da91fe61acb70d0f16db3e5c061
bendezoli <zoltan@octopus-digital.pro>
2024-04-23 14:14:51 +0300

    contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+// "use client";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Contact Page",
+  description: "Contact description",
+};
 
 const ContactPage = () => {
-  return <div>Contact page</div>;
+  return (
+    <div className={"flex flex-col lg:flex-row gap-20 pt-[40px] h-screen"}>
+      <div className="flex-1 self-center">
+        <div className={"relative aspect-square max-w-[500px] "}>
+          <Image src="/png/contact.png" alt="" fill className={""} />
+        </div>
+      </div>
+      <div className={"flex-1 self-center"}>
+        <form action="" className={"flex flex-col gap-5"}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
