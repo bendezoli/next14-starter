43c52ef396ef27fa3d22688e3c29677ddc85b8dc
bendezoli <zoltan@octopus-digital.pro>
2024-04-24 14:15:03 +0300

    fetching data part 1

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,34 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 
-const BlogPage = () => {
+const BlogPage = async () => {
   // { params, searchParams }
   // console.log(params);
   // console.log(searchParams);
 
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      next: { revalidate: 3600 }, // 60 minutes
+    });
+    // const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    //   cache: "no-store",
+    // });
+
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+
+    return res.json();
+  };
+
+  const posts = await getData();
+
   return (
     <div className={styles.container}>
       <div className={"w-full flex flex-wrap gap-4"}>
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        {posts.map((post, index) => (
+          <PostCard post={post} key={index} />
+        ))}
       </div>
     </div>
   );
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -5,19 +5,17 @@ import Link from "next/link";
 const PostCard = ({ post }) => {
   return (
     <div className={(styles.container, `w-full md:w-[48%] lg:w-[32%]`)}>
+      {/* <pre>{JSON.stringify(post, null, 2)}</pre> */}
       <div className={styles.top}>
         <div className={styles.imgContainer}>
           <Image src="/png/noavatar.png" alt="" fill className={styles.img} />
         </div>
-
-        {/* <span className={styles.date}>
-          {post.createdAt?.toString().slice(4, 16)}
-        </span> */}
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>This is a title</h1>
-        <p className={styles.desc}>lorem ipsum dolor sitamet</p>
-        <Link className={styles.link} href={"/blog/post"}>
+        <h1 className={styles.title}>{post.title.toUpperCase()}</h1>
+        <p className={styles.desc}>{post.body}</p>
+
+        <Link className={styles.link} href={`/blog/${post.id}`}>
           READ MORE
         </Link>
       </div>
