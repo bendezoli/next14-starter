cf2f5a757d1bda51f4878e433b195b08ba232f25
bendezoli <zoltan@octopus-digital.pro>
2024-05-06 11:59:14 +0300

    admin apge further developed

--- src/components/adminPosts/adminPosts.jsx
+++ src/components/adminPosts/adminPosts.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import styles from "./adminPosts.module.css";
+import { getPosts } from "@/lib/data";
+import Image from "next/image";
+import { deletePost } from "@/lib/action";
 
-const AdminPosts = () => {
-  return <div className={styles.container}>adminPosts</div>;
+const AdminPosts = async () => {
+  const posts = await getPosts();
+  const defaulIMG = "/png/noavatar.png";
+
+  return (
+    <div className={styles.container}>
+      <h1>Posts</h1>
+
+      {posts.map((post, postIndex) => (
+        <div className={styles.post} key={postIndex}>
+          <div className={styles.detail}>
+            <div className="w-[50px] h-[50px] relative">
+              <Image
+                src={post.img ? post.img : defaulIMG}
+                alt=""
+                fill
+                className={(styles.img, "object-cover")}
+              />
+            </div>
+
+            <span className={styles.postTitle}>{post.title}</span>
+          </div>
+          <form action={deletePost}>
+            <input type="hidden" name="id" value={post.id} />
+            <button className={styles.postButton}>Delete</button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AdminPosts;
--- src/components/adminUsers/adminUsers.jsx
+++ src/components/adminUsers/adminUsers.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import styles from "./adminUsers.module.css";
+import { getUsers } from "@/lib/data";
+import Image from "next/image";
+import { deleteUser } from "@/lib/action";
 
-const AdminUsers = () => {
-  return <div className={styles.container}>adminPosts</div>;
+const AdminUsers = async () => {
+  const users = await getUsers();
+  const defaulIMG = "/png/noavatar.png";
+  console.log(users);
+
+  return (
+    <div className={styles.container}>
+      <h1>Users</h1>
+      {users.map((user) => (
+        <div className={styles.user} key={user.id}>
+          <div className={styles.detail}>
+            <Image
+              src={user.img ? user.img : defaulIMG}
+              alt=""
+              width={50}
+              height={50}
+            />
+            <span>{user.username}</span>
+          </div>
+          <form action={deleteUser}>
+            <input type="hidden" name="id" value={user.id} />
+            <button className={styles.userButton}>Delete</button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AdminUsers;
